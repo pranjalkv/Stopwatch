6250dc4e7d913009434e2f1be7a35029352d3f18
PK Warlock <pyush0908@gmail.com>
2023-02-25 23:48:16 +0530

    second commit

--- index.js
+++ index.js
@@ -1,61 +1,62 @@
-let mili = 0;
-let seco = 0;
-let minu = 0;
-let hour = 0;
-let timer=false;
+let timer=document.querySelector(".clock");
+let timeNow;
+let t=0;
+let lapArray=[];
+let laps=document.querySelector(".laps")
 
-
-function stopwatch() 
+document.querySelector("#startb").addEventListener("click",function()
 {
-    if (timer) 
-    { 
-            mili++;
-            if (mili == 100) 
-            {
-                mili = 0;
-                seco++;
-
-            }
-           
-
-            if (seco == 60) {
-                seco = 0;
-                minu++;
-            }
-            
-            if (minu == 60) {
-                minu = 0;
-                hour++;
-
-            }
-              
-            document.querySelector(".hrs").innerHTML = hour;
-            document.querySelector(".min").innerHTML = minu;
-            document.querySelector(".sec").innerHTML = seco;
-            document.querySelector(".mil").innerHTML = mili;
-
-            setTimeout(stopwatch,10);
-        }
-  
-}
-document.querySelector("#startb").addEventListener("click", function () {
-    timer=true;
-    stopwatch();
-})
-document.querySelector("#stopb").addEventListener("click", function () 
-{
-    timer = false;
-})
-document.querySelector("#reset").addEventListener("click", function () 
-{
-    hour=0;
-    minu=0;
-    seco=0;
-    mili=0;
-            document.querySelector(".hrs").innerHTML = hour;
-            document.querySelector(".min").innerHTML = minu;
-            document.querySelector(".sec").innerHTML = seco;
-            document.querySelector(".mil").innerHTML = mili;
-
-    timer=false
+    timeNow = Date.now() - t;
+    timeAt = setInterval(timeUpdate, 10);
 });
+document.querySelector("#stopb").addEventListener("click",function()
+{
+    clearInterval(timeAt)
+    t=Date.now() - timeNow;
+})
+document.querySelector("#reset").addEventListener("click",function()
+{
+    clearInterval(timeAt)
+    timer.textContent="00:00:00:00";
+    laps.innerHTML="";
+    t=0;
+    lapArray=[];
+});
+function timeUpdate() {
+    t = Date.now() - timeNow;
+    let tdisplay = timeWritter(t);
+    timer.textContent = tdisplay;
+}
+
+function timeWritter(time) {
+    let date = new Date(time);
+    let hour=date.getUTCHours();
+    let min = date.getUTCMinutes();
+    let sec = date.getUTCSeconds();
+    let mili = Math.floor(date.getUTCMilliseconds() / 10);
+
+    hour=(hour<10)?"0"+hour:hour;
+    min =(min<10)?"0"+min:min;
+    sec = (sec<10)?"0"+ sec:sec;
+    mili =(mili <10)?"0"+mili:mili;
+
+    return hour+":"+min+":"+sec +":"+mili;
+}
+document.querySelector("#lapset").addEventListener("click",function()
+{
+    let currLap=t;
+    lapArray.push(currLap);
+    lapUpdate();
+})
+function lapUpdate()
+{
+    laps.innerHTML="";
+    for(let i=0;i<lapArray.length;i++)
+    {
+        currLap=lapArray[i];
+        let lapDisplay=timeWritter(currLap);
+        let newLap=document.createElement("div");
+        newLap.textContent="Lap"+(i+1)+"-"+lapDisplay;
+        laps.appendChild(newLap);
+    }
+}
